46f4d34e07b3bf0a21f4ed75944ba77f3ea69496
RossTarrant <rosstarrant3@gmail.com>
2022-02-20 17:35:59 +0000

    Created version 1 of library app

---
+++ script.js
@@ -0,0 +1,163 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        let bookInfo = `${this.title} by ${this.author}, ${this.pages} pages.`;
+        return bookInfo;
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  let haveRead = "";
+  if(read==="on"){
+      haveRead = true;
+  }
+  else{
+      haveRead = false;
+  }
+  newBook = new Book(title, author, pages, haveRead);
+  myLibrary.push(newBook);
+}
+
+function displayBooks(){
+    const content = document.querySelector(".content");
+    for (let pos in myLibrary) {
+        let newCard = document.createElement("div");
+        newCard.classList.add("card")
+        let title = document.createElement("h1");
+        title.textContent = `${myLibrary[pos].title}`;
+        let author = document.createElement("h2");
+        author.textContent = `by ${myLibrary[pos].author}`;
+        let pages = document.createElement("p");
+        pages.textContent = `${myLibrary[pos].pages} pages`;
+        let read = document.createElement("p");
+        let haveRead = "";
+        if(myLibrary[pos].read===true){
+            haveRead = "Read"
+            read.classList.add("read");
+        }
+        else{
+            haveRead = "Not Read";
+            read.classList.add("not-read");
+        }
+        read.textContent = `${haveRead}`;
+
+        let icons = document.createElement("div")
+
+        let toggleIcon = document.createElement("img");
+        toggleIcon.setAttribute("src", "toggle.svg")
+        toggleIcon.classList.add("icon")
+        toggleIcon.classList.add("toggle")
+        icons.appendChild(toggleIcon)
+        toggleIcon.addEventListener("click", function(){
+            if(myLibrary[pos].read===true){
+                myLibrary[pos].read = false;
+            }
+            else{
+                myLibrary[pos].read = true;
+            }
+            resetDisplay();
+            displayBooks();
+        })
+
+        let trashIcon = document.createElement("img");
+        trashIcon.setAttribute("src", "trash.svg")
+        trashIcon.classList.add("icon")
+        trashIcon.classList.add("trash")
+        icons.appendChild(trashIcon)
+        trashIcon.addEventListener("click", function(){
+            myLibrary.splice(pos, 1)
+            resetDisplay();
+            displayBooks();
+        })
+
+        newCard.appendChild(title);
+        newCard.appendChild(author);
+        newCard.appendChild(pages);
+        newCard.appendChild(read);
+        newCard.appendChild(icons)
+        content.appendChild(newCard);
+    }
+}
+
+function resetDisplay(){
+    const title = document.querySelector(".title")
+    const author = document.querySelector(".author")
+    const pages = document.querySelector(".pages")
+    const read = document.querySelector(".read")
+    title.value = "";
+    author.value = "";
+    pages.value = 0;
+    read.checked = false;
+    const cardNodes = document.querySelector(".content").childNodes;
+    for(let i=cardNodes.length-1 ; i >= 0; i--){
+        let cardNode = cardNodes[i]
+        cardNode.remove();
+    }
+}
+
+function addBook(){
+    const addBookForm = document.forms.addBookForm;
+    const formData = new FormData(addBookForm);
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = formData.get("pages");
+    const read = formData.get("read");
+    if(validateForm()===true){
+        addBookToLibrary(title, author, pages, read);
+        resetDisplay();
+        displayBooks();
+        closeForm();
+    }
+}
+
+function validateForm(){
+    const title = document.querySelector(".title")
+    const author = document.querySelector(".author")
+    const pages = document.querySelector(".pages")
+    const read = document.querySelector(".read")
+    if(title.value != "" && author.value != ""){
+        title.setAttribute("style", "border:1px solid black")
+        author.setAttribute("style", "border:1px solid black")
+        return true;
+    }
+    else{
+        if (title.value === ""){
+            title.setAttribute("style", "border:1px solid red")
+        }
+        if (author.value === ""){
+            author.setAttribute("style", "border:1px solid red")
+        }
+        return false;
+    }
+}
+
+function openForm() {
+    document.getElementById("addForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("addForm").style.display = "none";
+}
+
+addBookToLibrary("Dune", "Frank Herbert", 412, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+addBookToLibrary("Dune", "Frank Herbert", 412, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+addBookToLibrary("Dune", "Frank Herbert", 412, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+addBookToLibrary("Dune", "Frank Herbert", 412, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+addBookToLibrary("Dune", "Frank Herbert", 412, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+displayBooks();
+
+
+const addBtn = document.querySelector(".add-button");
+addBtn.addEventListener("click", function(){
+    openForm();
+})
